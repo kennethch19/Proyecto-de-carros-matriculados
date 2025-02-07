b4e0ca748d09adaa584d8b084520b8849680e64d
Dylan-Layedra <dflayedra@espe.edu.ec>
2025-02-07 12:11:34 -0500

    presentacion

--- priciapl.cpp
+++ priciapl.cpp
@@ -1,1 +1,34 @@
 #include<iostream>
+#include<windows.h>
+#include <conio.h>
+
+using namespace std;
+
+void gotoxy(int x, int y) {
+    HANDLE hcon;
+    hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x;
+    dwPos.Y = y;
+    SetConsoleCursorPosition(hcon, dwPos);
+}
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+
+int main(){
+
+    system("cls");
+    setColor(4);
+    gotoxy(40, 0); cout << "UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE";
+    setColor(2);
+    gotoxy(0, 2); cout << "REGISTRO DE CARROS MATRICULADOS";
+    setColor(3);
+    gotoxy(0, 4); cout << "Integrantes:DYLAN FERNANDO LAYEDRA JACOME.KENNETH MIJAEL CHICAIZA MIÑO";
+    gotoxy(0, 6); cout << "NRC:4598";
+    setColor(5);
+    system("pause");
+
+    return 0;
+}
